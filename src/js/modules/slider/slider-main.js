430a3b0880c99b33b5b69977fa32dcd4570c26c6
Artem <lunin.mail@gmail.com>
2022-01-23 10:16:59 +0200

    Lesson 33

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -40,27 +40,47 @@ export default class MainSlider extends Slider {
 		this.showSlides(this.slideIndex += n);
 	}
 
+	bindTriggers() {
+		this.btns.forEach(item => {
+			item.addEventListener('click', () => {
+				this.plusSlides(1);
+			});
+
+			// находим выше по DOM дереву ссылку перехода на первый слайд и вешаем на нее событие
+			item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+				e.preventDefault();
+				this.slideIndex = 1;
+				this.showSlides(this.slideIndex);
+			});
+		});
+		document.querySelectorAll('.prevmodule').forEach(item => {
+			item.addEventListener('click', (e) => {
+				e.stopPropagation();
+				e.preventDefault();
+				this.plusSlides(-1);
+			});
+		});
+
+		document.querySelectorAll('.nextmodule').forEach(item => {
+			item.addEventListener('click', (e) => {
+				e.stopPropagation();
+				e.preventDefault();
+				this.plusSlides(1);
+			});
+		});
+	}
+
 	render() {
-		try {
+		if (this.container) {
 			try {
 				//получаем всплывающий блок (главная страница, 3-й слайд)
 				this.hanson = document.querySelector('.hanson');
 			} catch(e){}
 
-			this.btns.forEach(item => {
-				item.addEventListener('click', () => {
-					this.plusSlides(1);
-				});
-
-				// находим выше по DOM дереву ссылку перехода на первый слайд и вешаем на нее событие
-				item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-					e.preventDefault();
-					this.slideIndex = 1;
-					this.showSlides(this.slideIndex);
-				});
-			});
-
 			this.showSlides(this.slideIndex);
-		} catch (e) {}
+			this.bindTriggers();
+
+			
+		}
 	}
 }
